60ffebab9ee8c71473ff6507fe58c4edfa1695c4
Boris <b.tseytlin@lambda-it.ru>
2021-03-21 18:52:19 +0300

    Fighting Heroku

--- tgmarkdown/config.py
+++ tgmarkdown/config.py
@@ -4,6 +4,6 @@ from confi import BaseEnvironConfig, ConfigField, IntConfig
 class Config(BaseEnvironConfig):
     TG_API_TOKEN = ConfigField(required=True)
     ADMIN_UID = IntConfig(required=True)
-    WEBHOOK_PORT = IntConfig(required=False)
+    PORT = IntConfig(required=False)
     HEROKU_NAME = ConfigField(required=False)
 
